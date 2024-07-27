408e4fe62ba9463dfb08fb3266ee049d1e87df4c
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-07-27 18:35:09 +0530

    updated disclaimer check

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
+// Show the disclaimer box when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('disclaimerBox').style.display = 'flex';
+});
+
 document
     .getElementById('resumeForm')
     .addEventListener('submit', function (event) {

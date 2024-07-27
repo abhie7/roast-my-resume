00ad415e1ef527cb4965cc1732db687e42aa25ed
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-07-27 18:55:14 +0530

    hotfix: disclaimer button

--- static/js/disclaimer.js
+++ static/js/disclaimer.js
@@ -1,5 +1,12 @@
 function acceptDisclaimer() {
     console.log('Accept button clicked');
     const disclaimerBox = document.getElementById('disclaimerBox');
-    disclaimerBox.setAttribute('style', 'display: none !important;');
+
+    // Set display to flex first
+    disclaimerBox.setAttribute('style', 'display: flex !important;');
+
+    // Use a timeout to change the display to none after a short delay
+    setTimeout(() => {
+        disclaimerBox.setAttribute('style', 'display: none !important;');
+    }, 100);
 }

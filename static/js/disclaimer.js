c0c63a03aaa1566c07c4d964c8efb5d64ba9c5dc
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-07-27 18:49:14 +0530

    hotfix: disclaimerBox

--- static/js/disclaimer.js
+++ static/js/disclaimer.js
@@ -1,6 +1,5 @@
 function acceptDisclaimer() {
     console.log('Accept button clicked');
     const disclaimerBox = document.getElementById('disclaimerBox');
-    console.log(disclaimerBox); // Should log the element
-    disclaimerBox.style.display = 'none';
+    disclaimerBox.setAttribute('style', 'display: none !important;');
 }

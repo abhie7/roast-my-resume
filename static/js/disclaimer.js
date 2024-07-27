a8567d3609a7111b8cc4bce44927ca7bcb4dea42
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-07-27 18:30:33 +0530

    update js, add disclaimer check

--- static/js/disclaimer.js
+++ static/js/disclaimer.js
@@ -1,4 +1,5 @@
 function acceptDisclaimer() {
     console.log('Accept button clicked');
     document.getElementById('disclaimerBox').style.display = 'none';
+    document.querySelector('.container').style.display = 'flex';
 }
